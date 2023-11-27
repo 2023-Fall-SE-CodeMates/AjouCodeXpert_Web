d6599be966e69c0d8bd09377f32a6470ce839565
Sungcheol Heo <suprlux09@gmail.com>
2023-11-27 18:39:59 +0900

    공지사항 추가/수정 페이지 추가

--- src/pages/common/SignUpPage.jsx
+++ src/pages/common/SignUpPage.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import style from "styles/pages/common/SignUpPage.module.css";
 import cn from "classnames";
-import { Formik, Form, Field, FieldArray } from "formik";
+import { Formik, Form, Field } from "formik";
 import { Link } from "react-router-dom";
 import * as Yup from "yup";
 
