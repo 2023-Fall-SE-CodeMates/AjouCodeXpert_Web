80138f4303fe7258c1bf6f57d36307821f43b525
Sungcheol Heo <suprlux09@gmail.com>
2023-12-06 23:42:22 +0900

    문제 페이지(학생) 실행 또는 제출 버튼 클릭 시 onSubmit에서 분기처리 및 버튼 비활성화

--- src/pages/student/ProblemPage.jsx
+++ src/pages/student/ProblemPage.jsx
@@ -16,6 +16,8 @@ function ProblemPage(props) {
   // 문제 정보
   // {language: 언어, points: 배점, explanation: 문제설명, tc: 테스트케이스}
   const [problemInfo, setProblemInfo] = useState({});
+
+  // 문제 정보 받아오기
   useEffect(() => {
     setProblemInfo({
       language: "c",
@@ -28,6 +30,30 @@ function ProblemPage(props) {
       ],
     });
   }, []);
+
+  // 학생이 작성하는 코드
+  const [code, setCode] = useState("");
+
+  // 학생 코드 정보 받아오기
+  useEffect(() => {
+    // 학생 코드 정보 받아오기
+
+    // 받아올 코드가 없을 시 기본 코드 설정
+    if (problemInfo.language === "c")
+      setCode("#include <stdio.h>\nint main() {\n\n}");
+    else if (problemInfo.language === "java")
+      setCode(
+        "public class Main {\n\tpublic static void main(String[] args) {\n\n\t}\n}"
+      );
+    else if (problemInfo.language === "python")
+      setCode("def main():\n\t\n\nif __name__ == '__main__':\n\tmain()");
+  }, [problemInfo]);
+
+  // 코드 실행 결과
+  const [result, setResult] = useState("");
+
+  // 제출 시 true, 실행 시 false
+  let isSubmit = true;
 
   return JSON.stringify(problemInfo) === "{}" ? null : (
     <div className="container px-1 d-flex flex-column vh-100">
@@ -104,16 +130,20 @@ function ProblemPage(props) {
       >
         <Formik
           initialValues={{
-            code:
-              problemInfo.language === "c"
-                ? "#include <stdio.h>\nint main() {\n\n}"
-                : problemInfo.language === "java"
-                ? "public class Main {\n\tpublic static void main(String[] args) {\n\n\t}\n}"
-                : "def main():\n\t\n\nif __name__ == '__main__':\n\tmain()",
+            code,
           }}
           enableReinitialize={true}
-          onSubmit={(data) => {
-            console.log(data);
+          onSubmit={async (data) => {
+            // 제출 시
+            if (isSubmit) {
+              await new Promise((resolve) => setTimeout(resolve, 2000));
+              setResult("제출 완료!");
+              console.log(data);
+            } else {
+              await new Promise((resolve) => setTimeout(resolve, 2000));
+              setResult("실행 완료!");
+              console.log(data);
+            }
           }}
         >
           {(props) => (
@@ -121,17 +151,35 @@ function ProblemPage(props) {
               <div className="d-flex flex-row justify-content-end mb-2">
                 <button
                   type="button"
-                  className="btn btn-outline-secondary me-2"
+                  className={`btn btn-outline-secondary me-2 ${
+                    props.isSubmitting ? "disabled" : ""
+                  }`}
+                  onClick={() => {}}
                 >
                   저장
                 </button>
                 <button
                   type="button"
-                  className="btn btn-outline-secondary me-2"
+                  className={`btn btn-outline-secondary me-2 ${
+                    props.isSubmitting ? "disabled" : ""
+                  }`}
+                  onClick={() => {
+                    isSubmit = false;
+                    props.handleSubmit();
+                  }}
                 >
                   실행
                 </button>
-                <button type="submit" className="btn btn-outline-primary">
+                <button
+                  type="button"
+                  className={`btn btn-outline-primary ${
+                    props.isSubmitting ? "disabled" : ""
+                  }`}
+                  onClick={() => {
+                    isSubmit = true;
+                    props.handleSubmit();
+                  }}
+                >
                   제출
                 </button>
               </div>
@@ -160,7 +208,9 @@ function ProblemPage(props) {
             </Form>
           )}
         </Formik>
-        <div className={cn("rounded-3 px-2 py-2", style.resultBox)}>결과</div>
+        <div className={cn("rounded-3 px-2 py-2", style.resultBox)}>
+          {result}
+        </div>
       </div>
     </div>
   );
