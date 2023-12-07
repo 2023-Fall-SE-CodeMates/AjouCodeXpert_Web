124035628969ec24d2bdf308af7ef1a41681a79f
Sungcheol Heo <suprlux09@gmail.com>
2023-12-07 19:29:06 +0900

    API 추가

--- src/services/api.js
+++ src/services/api.js
@@ -4,17 +4,6 @@ export const apiClient = axios.create({
   baseURL: "http://localhost:8080",
 });
 
-// export const retrieveAllTodosForUsernameApi = (username) =>
-//   apiClient.get(`/users/${username}/todos`);
-// export const deleteTodoApi = (username, id) =>
-//   apiClient.delete(`/users/${username}/todos/${id}`);
-// export const retrieveTodoApi = (username, id) =>
-//   apiClient.get(`/users/${username}/todos/${id}`);
-// export const updateTodoApi = (username, id, todo) =>
-//   apiClient.put(`/users/${username}/todos/${id}`, todo);
-// export const createTodoApi = (username, todo) =>
-//   apiClient.post(`/users/${username}/todos`, todo);
-
 // 공통
 // 로그인 페이지: 로그인
 export const authenticateApi = (id, pw) =>
@@ -43,5 +32,66 @@ export const createClassJoinRequestApi = (id, role, classCode) => {
 export const retrieveClassListApi = (id) =>
   apiClient.get(`/api/v1/class?joinedBy=${id}`);
 
+// 개인 페이지: 개인정보 불러오기, 개인정보 수정, 회원 탈퇴 요청, 알림 목록 불러오기, 알림 삭제
+export const retrieveMyInfoApi = (id) =>
+  apiClient.get(`/api/v1/user/info?id=${id}`);
+
+export const updateMyInfoApi = (id, pw, name, studentCode, majorCode) =>
+  apiClient.put(`/api/v1/user/info?id=${id}`, {
+    pw,
+    name,
+    studentCode,
+    majorCode,
+  });
+
+export const deleteAccountApi = (id) =>
+  apiClient.delete(`/api/v1/user/info?id=${id}`);
+
+export const retrieveNotificationListApi = (id) =>
+  apiClient.get(`/api/v1/alarm?receiver=${id}&read=0`);
+
+export const deleteNotificationApi = (alarmId) =>
+  apiClient.patch(`/api/v1/alarm/${alarmId}?read=1`);
+
+// 공지 페이지: 공지 목록 불러오기, 공지 삭제(TA)
+export const retrieveNoticeListApi = (classId) =>
+  apiClient.get(`/api/v1/class/${classId}/notice`);
+
+export const deleteNoticeApi = (classId, noticeId) =>
+  apiClient.delete(`/api/v1/class/${classId}/notice/${noticeId}`);
+
+// 과제 페이지: 과제 목록 불러오기, 과제 삭제(TA)
+export const retrieveAssignmentListApi = (classId) =>
+  apiClient.get(`/api/v1/class/${classId}/homework`);
+
+// 성적 페이지: 성적 목록 불러오기
+
+// TA
 // 반 개설 페이지
 export const createClassApi = () => apiClient.post("/api/v1/class");
+
+// 구성원 관리 페이지: 반 코드 불러오기, 구성원 목록 불러오기, 참여 요청 불러오기, 참여 요청 승인/거부
+export const retrieveClassCodeApi = (classId) =>
+  apiClient.get(`/api/v1/class/${classId}/code`);
+
+export const retrieveMembersApi = (classCode) =>
+  apiClient.get(`/api/v1/class/join?classCode=${classCode}&joined=1`);
+
+export const retrieveRequestersApi = (classCode) =>
+  apiClient.get(`/api/v1/class/join?classCode=${classCode}&joined=0`);
+
+export const acceptRequestApi = (id) =>
+  apiClient.patch(`/api/v1/class/join/${id}&joined=1`);
+
+// 공지사항 추가/수정 페이지: 공지사항 추가, 공지사항 수정
+export const createNotificationApi = (classId, title, content) =>
+  apiClient.post(`/api/v1/class/${classId}/notice`, {
+    title,
+    content,
+  });
+
+export const updateNotificationApi = (classId, noticeId, title, content) =>
+  apiClient.put(`/api/v1/class/${classId}/notice/${noticeId}`, {
+    title,
+    content,
+  });
